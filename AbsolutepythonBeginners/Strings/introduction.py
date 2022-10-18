0897d6a1fec07308d1651a8327626fcab9d7b1f0
charan aerospace <scaerospace30@gmail.com>
2022-10-18 19:11:57 +0530

    added new folder

---
+++ AbsolutepythonBeginners/Strings/introduction.py
@@ -0,0 +1,13 @@
+quote="I think there is a world market for maybe five computers."
+print(quote)
+print(len(quote))
+print(type(quote))
+
+# UpperCase
+print(quote.upper())
+# LowerCase
+print(quote.lower())
+# title
+print(quote.title())
+# replace
+print(quote.replace("five","millions of"))
